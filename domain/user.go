827104b346f4246b1047673c9106966d71e1ce4d
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-02-20 18:40:12 +0900

    feat: userドメインとuser_repositoryのインターフェース作成

---
+++ domain/user.go
@@ -0,0 +1,57 @@
+package domain
+
+import "errors"
+
+type User struct {
+	id    UserId
+	name  UserName
+	token UserToken
+}
+
+func (u User) GetId() UserId {
+	return u.id
+}
+
+func (u User) GetName() UserName {
+	return u.name
+}
+
+func (u User) SetName(name UserName) {
+	u.name = name
+}
+
+func (u User) GetToken() UserToken {
+	return u.token
+}
+
+func NewUser(id UserId, name UserName, token UserToken) User {
+	return User{id, name, token}
+}
+
+type UserId int
+type UserName string
+type UserToken string
+
+func NewId(value int) (*UserId, error) {
+	if value < 1 {
+		return nil, errors.New("idは1以上の整数である必要があります。")
+	}
+	id := UserId(value)
+	return &id, nil
+}
+
+func NewName(value string) (*UserName, error) {
+	if len(value) < 1 {
+		return nil, errors.New(("nameは1文字以上である必要があります。"))
+	}
+	name := UserName(value)
+	return &name, nil
+}
+
+func NewToken(value string) (*UserToken, error) {
+	if len(value) < 1 {
+		return nil, errors.New(("tokenは1文字以上である必要があります。"))
+	}
+	token := UserToken(value)
+	return &token, nil
+}
