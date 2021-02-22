eae602b9dcd7d076affba10929e8d5c36fec1a18
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-02-22 17:25:53 +0900

    feat: add Update method

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -14,7 +14,7 @@ func NewUserRepository() repository_interface.IUserRepository {
 	return &userRepository{}
 }
 
-func (ur userRepository) Save(user *domain.User) error {
+func (ur userRepository) Insert(user *domain.User) error {
 	//DBの接続
 	//<user名>:<パスワード>@/<db名>
 	db, err := sql.Open("mysql", "root:example@/go_database")
@@ -23,12 +23,35 @@ func (ur userRepository) Save(user *domain.User) error {
 	}
 	defer db.Close()
 
-	rows, err := db.Prepare("INSERT INTO user VALUES (?, ?, ?)")
+	rows, err := db.Prepare("INSERT INTO users VALUES (?, ?, ?)")
 	if err != nil {
 		return err
 	}
 
 	_, err = rows.Exec(user.GetId(), user.GetName(), user.GetToken())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ur userRepository) Update(user *domain.User) error {
+	//DBの接続
+	//<user名>:<パスワード>@/<db名>
+	db, err := sql.Open("mysql", "root:example@/go_database")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	rows, err := db.Prepare("UPDATE users SET name=? WHERE id=? AND token=?")
+	if err != nil {
+		return err
+	}
+
+	// この辺型大丈夫なのかよくわからない
+	_, err = rows.Exec(user.GetName(), user.GetId(), user.GetToken())
 	if err != nil {
 		return err
 	}
@@ -48,7 +71,7 @@ func (ur userRepository) FindByToken(token *domain.UserToken) (*domain.User, err
 	var id int
 	var name string
 
-	err = db.QueryRow("SELECT id, name FROM user WHERE token=?", token).Scan(id, name)
+	err = db.QueryRow("SELECT id, name FROM users WHERE token=?", token).Scan(&id, &name)
 	if err != nil {
 		return nil, err
 	}
@@ -77,11 +100,11 @@ func (ur userRepository) GetMaxId() (*domain.UserId, error) {
 	defer db.Close()
 
 	var id int
-
-	err = db.QueryRow("SELECT MAX(id) FROM user").Scan(id)
+	err = db.QueryRow("SELECT MAX(id) FROM users").Scan(&id)
 	if err != nil {
 		return nil, err
 	}
+
 	userId, err := domain.NewId(id)
 	if err != nil {
 		return nil, err
