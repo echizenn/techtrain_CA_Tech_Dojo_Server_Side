0961b59133f9284d7439670016aac057e06d0b6f
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-02-23 17:14:39 +0900

    feat: make character/list API without userCharacterID

--- main.go
+++ main.go
@@ -20,6 +20,9 @@ func main() {
 
 	http.HandleFunc("/gacha/draw", gachaDraw)
 
+	// このURL微妙な感じがした
+	http.HandleFunc("/character/list", userHoldCharacterList)
+
 	// 8088ポートで起動
 	http.ListenAndServe(":8088", nil)
 }
@@ -180,5 +183,36 @@ func gachaDraw(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("results", string(stringResults))
+	w.WriteHeader(http.StatusOK)
+}
+
+func userHoldCharacterList(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed) // 405
+		// ここの処理怪しさしかない
+		w.Write([]byte("GETだけです。"))
+		return
+	}
+
+	header := r.Header
+	token := header["X-Token"][0] // なんで大文字になる？、0って明示して大丈夫？
+
+	ur := repository.NewUserRepository()
+	cr := repository.NewCharacterRepository()
+	ucr := repository.NewUsersCharactersRepository(cr)
+
+	ucas := application.NewUsersCharactersApplicationService(ur, ucr)
+
+	userHoldCharacters, err := ucas.Hold(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stringCharacters, err := json.Marshal(userHoldCharacters)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("characters", string(stringCharacters))
 	w.WriteHeader(http.StatusOK)
 }
